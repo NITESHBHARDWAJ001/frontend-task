d85cd038336aae44996e1881682d8791ea9faf97
Nitesh Bhardwaj <niteshsharmans775@gmail.com>
2026-03-14 18:06:20 +0530

    preview 3 added

--- src/data/playlistData.js
+++ src/data/playlistData.js
@@ -6,7 +6,7 @@ const playlistData = {
   duration: '1 hr 14 min',
   tags: ['Rock', 'Alt Rock'],
   playlistImage: '/Frame 65.png',
-  previewImages: ['/preview1.png', '/preview2.jpg', '/preview3.png'],
+  previewImages: ['/preview1.png', '/preview2.jpg', '/preview3.svg'],
 }
 
 export default playlistData
